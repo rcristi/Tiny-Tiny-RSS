7fe5b16db987f84add26baa096ee839c80a93e64
Cristian RAUTA <cristian.rauta@gmail.com>
2015-05-09 08:58:43 +0200

    enable api_newsplus

--- config.php
+++ config.php
@@ -197,7 +197,7 @@
 	// if you experience weird errors and tt-rss failing to start, blank pages
 	// after login, or content encoding errors, disable it.
 
-	define('PLUGINS', 'auth_internal, note');
+	define('PLUGINS', 'auth_internal, note, api_newsplus');
 	// System plugins have to be specified here. Please enable at least one
 	// authentication plugin here (auth_*).
 	// Users may enable other user plugins from Preferences/Plugins but may not
